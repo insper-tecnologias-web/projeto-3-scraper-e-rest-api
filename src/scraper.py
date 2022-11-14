e48847c8f8eb501a4d58759d6f506544064b3c2e
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2022-11-14 18:08:33 -0300

    iniciando scraping

---
+++ src/scraper.py
@@ -0,0 +1,12 @@
+import bs4
+from bs4 import BeautifulSoup
+
+# Se precisar de aleatoriedade de user agent:
+# pip install fake-useragent
+# from fake_useragent import UserAgent
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    
