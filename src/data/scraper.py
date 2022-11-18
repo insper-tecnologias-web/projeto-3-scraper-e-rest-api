2a2adea0220b66543fc15540355b57feb2370ecf
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2022-11-18 10:28:28 -0300

    Mudanças de qualidade de vida no scraper

--- src/data/scraper.py
+++ src/data/scraper.py
@@ -5,27 +5,36 @@ from bs4 import BeautifulSoup
 # pip install fake-useragent
 # from fake_useragent import UserAgent
 
+
 class Scraper:
     def __init__(self):
-        self.SCHEME = 'https://'
-        self.HOST:str = 'boardgamegeek.com'
-        self.MAIN_PAGE_BASE_PATH:str = '/browse/boardgame/page/'
-        self.BOARDGAME_PAGE_BASE_PATH:str = '/boardgame/'
+        self.SCHEME: str = 'https://'
+        self.HOST: str = 'boardgamegeek.com'
+        self.MAIN_PAGE_BASE_PATH: str = '/browse/boardgame/page/'
+        self.BOARDGAME_PAGE_BASE_PATH: str = '/boardgame/'
 
-        self.page:int = 1
+        self.page: int = 1
+
+    # Private instance methods
+    def _generateURL(self, path):
+        url = ''.join(tuple(self.SCHEME, self.HOST, path))
+        return url
+
+    def _getBoardgamePath(self, htmlTag_a):
         pass
 
-    def __generateURL(self, path):
-        return self.SCHEME + self.HOST + path
-
-    def getBoardgameURI(self, htmlTag_a):
+    def _navigateToBoargamePage(self, boardgame_path):
         pass
 
-    def navigateToBoargamePage(self, boardgame_path):
+    def _getGameDuration(self, boardgame_path):
         pass
 
-    def getGameDuration(self, boardgame_path):
+    def _scrapMainPage(self):
         pass
 
-    def scrap(self):
+    def _scrapBoardgamePage(self, boardgame_path):
+        pass
+
+    # Public instace method
+    def run(self):
         pass
